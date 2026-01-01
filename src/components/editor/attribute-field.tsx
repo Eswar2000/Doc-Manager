020d0ba0d8bb2f4dbfc20ff60ee423de71e36758
Eswar2000 <v.eswarraman2000@gmail.com>
2026-01-01 20:20:50 +0530

    cleaner styling of the attribute field

--- src/components/editor/attribute-field.tsx
+++ src/components/editor/attribute-field.tsx
@@ -36,19 +36,26 @@ export const AttributeField = Node.create({
       "span",
       {
         "data-attribute-field": "",
-        style: `
-          font-size: inherit;
-          line-height: inherit;
-          font-weight: inherit;
-          font-style: inherit;
-          vertical-align: baseline;
-          display: inline;
-        `,
-        class:
-          "mx-1 px-2 bg-blue-100 text-blue-800 border border-dashed border-blue-500 rounded select-none cursor-pointer",
+        contenteditable: "false", // Fixes cursor trapping inside
+        style: "font: inherit; vertical-align: middle;",
+        class: "inline-block align-middle mx-1 px-2 py-px bg-blue-100 text-blue-800 border border-dashed border-blue-500 rounded select-none cursor-pointer leading-none",
       },
       `{{ ${label} }}`,
     ];
+  },
+
+  addNodeView() {
+    return ({ node }) => {
+      const dom = document.createElement("span");
+      dom.textContent = `{{ ${node.attrs.label} }}`;
+      dom.contentEditable = "false";
+      dom.className = "inline-block align-middle mx-1 px-2 py-px bg-blue-100 text-blue-800 border border-dashed border-blue-500 rounded select-none cursor-pointer leading-none";
+      dom.setAttribute("data-attribute-field", "");
+      // ensure it inherits the surrounding font and is vertically centered in the line
+      dom.style.font = "inherit";
+      dom.style.verticalAlign = "middle";
+      return { dom };
+    };
   },
 
   addCommands() {
