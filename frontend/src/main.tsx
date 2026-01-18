83bd04493e2a586686aef856bf5fe267aeaf7b10
Eswar2000 <v.eswarraman2000@gmail.com>
2026-01-19 00:32:18 +0530

    templates page updated with tanstack query for periodic fetch and sync

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1, // retry once on failure
+      staleTime: 1000 * 60, // 1 minute data refresh interval
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 )
